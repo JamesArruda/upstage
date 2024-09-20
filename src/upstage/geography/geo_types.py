8dcb8a6a4ceee445c080d743df68613a5a1082b7
James Arruda <james.arruda@gtri.gatech.edu>
2024-09-20 10:09:40 -0400

    Flexible inputs for geography

--- src/upstage/geography/geo_types.py
+++ src/upstage/geography/geo_types.py
@@ -14,36 +14,57 @@ LAT_LON_ALT = POSITION
 LAT_LON = tuple[float, float]
 
 
+class GEO_POINT(Protocol):
+    """Protocol for mimicking geographic data."""
+
+    def latlon(self) -> LAT_LON:
+        """Return a tuple of latitude and longitude in degrees."""
+
+
+def _convert_geo(point: LAT_LON | GEO_POINT) -> LAT_LON:
+    """Convert a geo point (or Lat/Lon) to Lat/Lon.
+
+    Args:
+        point (LAT_LON | GEO_POINT): The position on a globe
+
+    Returns:
+        LAT_LON: Position as correct type.
+    """
+    if isinstance(point, tuple):
+        return point
+    return point.latlon()
+
+
 class EarthProtocol(Protocol):
     """Protocol for defining an earth model interface."""
 
     def distance(
         self,
-        loc1: LAT_LON,
-        loc2: LAT_LON,
+        loc1: LAT_LON | GEO_POINT,
+        loc2: LAT_LON | GEO_POINT,
         units: str,
     ) -> float:
         """Get the distance between two lat/lon (degrees) points."""
 
     def bearing(
         self,
-        loc1: LAT_LON,
-        loc2: LAT_LON,
+        loc1: LAT_LON | GEO_POINT,
+        loc2: LAT_LON | GEO_POINT,
         units: str,
     ) -> float:
         """Get the distance between two lat/lon (degrees) points."""
 
     def distance_and_bearing(
         self,
-        loc1: LAT_LON,
-        loc2: LAT_LON,
+        loc1: LAT_LON | GEO_POINT,
+        loc2: LAT_LON | GEO_POINT,
         units: str,
     ) -> tuple[float, float]:
         """Get the distance between two lat/lon (degrees) points."""
 
     def point_from_bearing_dist(
         self,
-        point: LAT_LON,
+        point: LAT_LON | GEO_POINT,
         bearing: float,
         distance: float,
         distance_units: str = "nmi",
@@ -64,15 +85,15 @@ class EarthProtocol(Protocol):
 
     def geo_linspace(
         self,
-        start: LAT_LON,
-        end: LAT_LON,
+        start: LAT_LON | GEO_POINT,
+        end: LAT_LON | GEO_POINT,
         num_segments: int,
     ) -> list[LAT_LON]:
         """Get evenly spaced coordinates between lat/lon pairs."""
 
     def geo_circle(
         self,
-        center: LAT_LON,
+        center: LAT_LON | GEO_POINT,
         radius: float,
         radius_units: str,
         num_points: int,
